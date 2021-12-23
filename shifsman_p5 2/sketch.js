7c7d14a3b0c8268f72fb11cb5a760a5cdc30dd78
cchangtheman <96593149+cchangtheman@users.noreply.github.com>
2021-12-23 18:29:32 +0000

    Add files via upload

---
+++ shifsman_p5 2/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+ createCanvas(640, 360); 
+}
+
+function draw() {
+    background(0,0,0);
+    fill(255,255,255, 100)
+ellipse(320, 230, 100);
+ellipse(320, 150, 70, 70);
+    ellipse(320, 100, 60, 60);
+    
+    fill(0, 0, 0);
+    ellipse(320, 250, 20, 20);
+    ellipse(320, 210, 20, 20);
+    ellipse(320, 150, 20, 20);
+    ellipse(300, 90, 10, 10);
+      ellipse(340, 90, 10, 10);
+fill(130, 20, 40)
+         ellipse(320, 110, 15, 15);
+}
+
+  
